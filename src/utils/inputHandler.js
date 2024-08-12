f6cdf74f60a6a8b012c372a0840a908b32d11111
seulgi <sgsg9447@gmail.com>
2024-08-13 01:19:11 +0900

    refactor: play 함수 분리

--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,4 +1,5 @@
 import readLine from 'readline';
+import { MIN_NUMBER, MAX_NUMBER } from '../constants/numbers.js';
 
 export function readLineAsync(query) {
   return new Promise((resolve, reject) => {
@@ -16,4 +17,14 @@ export function readLineAsync(query) {
       resolve(input);
     });
   });
+}
+
+export async function getUserInput() {
+  let input = await readLineAsync('숫자를 입력하세요: ');
+  input = parseInt(input, 10);
+  if (isNaN(input) || input < MIN_NUMBER || input > MAX_NUMBER) {
+    console.log(`${MIN_NUMBER}~${MAX_NUMBER} 사이의 숫자를 입력하세요.`);
+    return getUserInput();
+  }
+  return input;
 }
