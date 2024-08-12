471d8f3f4caf4a0d01aa236ecb680479cf40a084
seulgi <sgsg9447@gmail.com>
2024-08-13 00:10:55 +0900

    feat: 이전 추측 보여주는 기능 구현

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ async function play() {
   const randomNumber = generateRandomNumber();
   console.log(randomNumber);
   console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.');
+  const inputValue = [];
   while (true) {
     let input = await readLineAsync('숫자를 입력하세요: ');
     input = parseInt(input, 10);
+    inputValue.push(input);
     if (isNaN(input) || input < MIN_NUMBER || input > MAX_NUMBER) {
       console.log('1~50 사이의 숫자를 입력하세요.');
+      inputValue.pop();
       continue;
     }
-    checkGuessNumber(input, randomNumber);
+    checkGuessNumber(input, randomNumber, inputValue);
   }
 }
 
